b863757e0f03282ad4bd5acce90f5ba084c441f4
Bors Beckie <borsbeck@ob-j7.nsk.21-school.ru>
2024-07-07 15:44:43 +0700

    add draw of all objects, needs to add other objects

--- objects_2D.h
+++ objects_2D.h
@@ -13,9 +13,9 @@ class Point : public Object {
   void draw(Bitmap& bmp) override {
     if (x >= 0 && x < bmp.m_width && y >= 0 && y < bmp.m_height) {
       size_t idx = (y * bmp.m_width + x) * 3;
-      bmp.m_buffer[idx] = 255;      // Red
-      bmp.m_buffer[idx + 1] = 255;  // Green
-      bmp.m_buffer[idx + 2] = 255;  // Blue
+      bmp.m_buffer[idx] = 255;
+      bmp.m_buffer[idx + 1] = 0;
+      bmp.m_buffer[idx + 2] = 0;
     }
   }
 
@@ -26,7 +26,19 @@ class Point : public Object {
 class Rect : public Object {
  public:
   Rect(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {}
-  void draw(Bitmap& bmp) override {}
+  void draw(Bitmap& bmp) override {
+    for (int x = x1; x <= x2; ++x) {
+      for (int y = y1; y <= y2; ++y) {
+        if (x >= 0 && x < bmp.m_width && y >= 0 && y < bmp.m_height &&
+            (x == x1 || x == x2 || y == y1 || y == y2)) {
+          size_t idx = (y * bmp.m_width + x) * 3;
+          bmp.m_buffer[idx] = 255;
+          bmp.m_buffer[idx + 1] = 0;
+          bmp.m_buffer[idx + 2] = 0;
+        }
+      }
+    }
+  }
 
  private:
   int x1, y1, x2, y2;
@@ -35,7 +47,16 @@ class Rect : public Object {
 class HLine : public Object {
  public:
   HLine(int x1, int x2, int y) : x1(x1), x2(x2), y(y) {}
-  void draw(Bitmap& bmp) override {}
+  void draw(Bitmap& bmp) override {
+    for (int x = x1; x <= x2; ++x) {
+      if (x >= 0 && x < bmp.m_width && y >= 0 && y < bmp.m_height) {
+        size_t idx = (y * bmp.m_width + x) * 3;
+        bmp.m_buffer[idx] = 255;
+        bmp.m_buffer[idx + 1] = 0;
+        bmp.m_buffer[idx + 2] = 0;
+      }
+    }
+  }
 
  private:
   int x1, x2, y;
@@ -44,7 +65,16 @@ class HLine : public Object {
 class VLine : public Object {
  public:
   VLine(int y1, int y2, int x) : y1(y1), y2(y2), x(x) {}
-  void draw(Bitmap& bmp) override {}
+  void draw(Bitmap& bmp) override {
+    for (int y = y1; y <= y2; ++y) {
+      if (x >= 0 && x < bmp.m_width && y >= 0 && y < bmp.m_height) {
+        size_t idx = (y * bmp.m_width + x) * 3;
+        bmp.m_buffer[idx] = 255;
+        bmp.m_buffer[idx + 1] = 0;
+        bmp.m_buffer[idx + 2] = 0;
+      }
+    }
+  }
 
  private:
   int y1, y2, x;
